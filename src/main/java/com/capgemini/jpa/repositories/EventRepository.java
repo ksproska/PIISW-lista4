a89ffdba2d1d4ae79f46334bfcd0981266853a8b
Kamila Sproska <kamila.sproska@gmail.com>
2023-04-08 23:09:38 +0200

    Task 3 (1/2)

--- src/main/java/com/capgemini/jpa/repositories/EventRepository.java
+++ src/main/java/com/capgemini/jpa/repositories/EventRepository.java
@@ -4,6 +4,8 @@ import com.capgemini.jpa.entities.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 
@@ -11,4 +13,10 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Page<Event> findEventsByTimeBetweenAndAnalysisRequiredEquals(
             LocalDateTime start, LocalDateTime end, boolean toBeAnalyzed, Pageable pageable
     );
+
+    void deleteByTimeBefore(LocalDateTime givenDate);
+
+    @Modifying
+    @Query("update Event u set u.analysisRequired = true")
+    void queryAllByDurationGreaterThan(Class<? extends Event> clazz, int threshold);
 }
